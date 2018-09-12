34486bce845a4f0a5ea8c5c1340d41f29ad2a09c
s124609 <seth.hancock609@student.mckinneyisd.net>
2018-09-12 10:27:34 -0500

    do while loop

--- src/Passcode.java
+++ src/Passcode.java
@@ -6,78 +6,79 @@ public class Passcode
 {
     private static char[] vowels = {'a','e','i','o','u','A','E','I','O','U',',','.',' '};
     private static boolean[] prime;
+    private static String inputString;
 
     public static void main(String[] args) throws IOException
     {
         Scanner file = new Scanner(new File("passcode.dat"));
 
-        String inputString = file.nextLine();
-        char[] inputChar = inputString.toCharArray();
+        do {
+            inputString = file.nextLine();
+            char[] inputChar = inputString.toCharArray();
 
-        int pos = 0;
-        prime = makePrime();
+            int pos = 0;
+            prime = makePrime();
 
-        do{
-            //even && vowel
-            if (pos%2==0 && isVowel(inputChar[pos]))
-                inputChar[pos] = (("" + inputChar[pos]).toUpperCase()).charAt(0);
+            do {
+                //even && vowel
+                if (pos % 2 == 0 && isVowel(inputChar[pos]))
+                    inputChar[pos] = (("" + inputChar[pos]).toUpperCase()).charAt(0);
 
-            //prime && consonant
-            if (isPrime(pos) && !isVowel(inputChar[pos]))
-            {
-                //lowercase
-                if (inputChar[pos] >= 97 && inputChar[pos] <= 122)
-                {
-                    do{
-                        inputChar[pos] += 1;
-
-                        if (inputChar[pos] >= 122)
-                            inputChar[pos] = 98;
-
-                        if (isVowel(inputChar[pos]))
+                //prime && consonant
+                if (isPrime(pos) && !isVowel(inputChar[pos])) {
+                    //lowercase
+                    if (inputChar[pos] >= 97 && inputChar[pos] <= 122) {
+                        do {
                             inputChar[pos] += 1;
 
-                    }while (isVowel(inputChar[pos]));
+                            if (inputChar[pos] >= 122)
+                                inputChar[pos] = 98;
 
-                }
+                            if (isVowel(inputChar[pos]))
+                                inputChar[pos] += 1;
 
-                //uppercase
-                else if (inputChar[pos] >= 65 && inputChar[pos] <= 90)
-                {
-                    do{
-                        inputChar[pos] += 1;
+                        } while (isVowel(inputChar[pos]));
 
-                        if (inputChar[pos] == 90)
-                            inputChar[pos] = 66;
+                    }
 
-                        if (isVowel(inputChar[pos]))
+                    //uppercase
+                    else if (inputChar[pos] >= 65 && inputChar[pos] <= 90) {
+                        do {
                             inputChar[pos] += 1;
 
-                    }while (isVowel(inputChar[pos]));
+                            if (inputChar[pos] == 90)
+                                inputChar[pos] = 66;
 
+                            if (isVowel(inputChar[pos]))
+                                inputChar[pos] += 1;
+
+                        } while (isVowel(inputChar[pos]));
+
+                    }
                 }
+
+                //odd && consonant
+                else if (pos % 2 != 0 && !isVowel(inputChar[pos]))
+                    inputChar[pos] = '*';
+
+                pos++;
+
+            } while (pos < inputChar.length);
+
+            for (int i = 0; i < inputChar.length; i++) {
+                if (inputChar[i] == ' ')
+                    i++;
+                out.print(inputChar[i] + "");
             }
 
-            //odd && consonant
-            else if (pos%2!=0 && !isVowel(inputChar[pos]))
-                inputChar[pos] = '*';
-
-            pos++;
-
-        }while(pos < inputChar.length);
-
-        for (int i = 0; i < inputChar.length; i++)
-        {
-            if (inputChar[i] == ' ')
-                i++;
-            out.print(inputChar[i] + "");
-        }
+            out.println();
+        } while (file.hasNextLine());
     }
 
     static boolean[] makePrime()
     {
 
-        int max = 50;
+        int max = inputString.length();
         boolean[] primeAr = new boolean[max];
 
         for (int next = 2; next <= Math.sqrt(max); next++)
